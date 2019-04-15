eb6744417d638361dada0cf0180eb17178fe7f89
Cara Eppes <ceppes@udel.edu>
2019-04-15 19:24:29 -0400

    first deploy

--- tcp-ui/server.js
+++ tcp-ui/server.js
@@ -3,12 +3,41 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/MY_APP_NAME'));
+app.use(express.static(__dirname + '/dist/tcp-ui'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/MY_APP_NAME/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/tcp-ui/index.html'));
 });
 
 // default Heroku port
 app.listen(process.env.PORT || 5000);
+
+const express = require('express');
+const app = express();
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+
+const path = require('path');
+// ...
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
