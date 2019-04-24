e051d528e88da7d253a2cbf8c7edbfadf0161c1d
Kate Bayard <kfbayard@gmail.com>
2019-04-23 21:51:16 -0400

    connecting logout to server

--- tcp-ui/src/app/app.component.ts
+++ tcp-ui/src/app/app.component.ts
@@ -37,13 +37,16 @@ export class AppComponent {
     });
     this.loggedIn = this.session.retrieve("currentUser") != null;
   }
-  
+
   title = 'Chat Dragon';
 
   logout() {
-    // this.userService.logoutUser(this.currentUser.username).subscribe();
-    this.userService.changeCurrentUser(null);
-    this.session.store("currentUser", null);
+    this.currentUser = this.session.retrieve("currentUser");
+     this.userService.logoutUser(this.currentUser.username).subscribe(()=>{
+       this.userService.changeCurrentUser(null);
+       this.session.store("currentUser", null);
+     });
+
     this.notificationService.clear();
     this.router.navigate(['/login']);
   }
